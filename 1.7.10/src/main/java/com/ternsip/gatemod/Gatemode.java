71314e91579be733685a09e568671b6185ab12dc
ternsip <ternsip@gmail.com>
2016-04-10 21:20:30 +0300

    Fixed internal modversion

--- 1.7.10/src/main/java/com/ternsip/gatemod/Gatemode.java
+++ 1.7.10/src/main/java/com/ternsip/gatemod/Gatemode.java
@@ -41,7 +41,7 @@ public class Gatemode {
 
     public static final String MODID = "gatemod";
     public static final String MODNAME = "GateMod";
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.2";
     public static final String AUTHOR = "Ternsip";
     public static final String MCVERSION = "1.7.*";
 
